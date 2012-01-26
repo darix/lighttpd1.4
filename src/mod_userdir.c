916a2467b3d6bbb9b9f2658824ef14003e3d246a
Marcus Rückert <mrueckert@suse.de>
2012-01-26 13:31:27 +0100

    applied lighttpd-1.4.x_mod_userdir_disclosure.patch

--- src/mod_userdir.c
+++ src/mod_userdir.c
@@ -169,7 +169,12 @@ URIHANDLER_FUNC(mod_userdir_docroot_handler) {
 	if (con->uri.path->used == 0) return HANDLER_GO_ON;
 
 	mod_userdir_patch_connection(srv, con, p);
-	
+
+	/* enforce the userdir.path to be set in the config, ugly fix for #1587;
+	 * should be replaced with a clean .enabled option in 1.5
+	 */
+	if (p->conf.path->used == 0) return HANDLER_GO_ON;
+
 	uri_len = con->uri.path->used - 1;
 	
 	/* /~user/foo.html -> /home/user/public_html/foo.html */
